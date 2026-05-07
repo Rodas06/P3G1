874568f7669c4bc7bc11975d6981a93ba185f29b
RodrigoOliveira <rodrigocastillo@ua.pt>
2026-05-07 14:37:56 +0100

    Adicionei algumas classes

--- P3G1_ProjetoFinal.cpp
+++ P3G1_ProjetoFinal.cpp
@@ -1,8 +1,47 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+class User {
+
+    private:
+    string Nome, Email, Telemovel;
+
+    public:
+
+    User(string nome, string email, string tele): Nome(nome), Email(email), Telemovel(tele){}
+    
+};
+
+class Msg {
+    private:
+
+    string Info;
+
+    public:
+
+    Msg (string info): Info(info){}
+
+    virtual void getType()=0;
+};
+
+class EmailMsg: public Msg{
+
+    private:
+
+    unsigned int ID;
+
+    public:
+
+    EmailMsg(string info, unsigned int id): Msg(info), ID(id){}
+
+    
+
+};
+
 int main (){
-  
-return 0;
+
+
+
 }
