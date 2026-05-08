5ba665329a7e7d16dbf77b98192e25bf2c52afec
RodrigoOliveira <rodrigocastillo@ua.pt>
2026-05-08 19:26:05 +0100

    Adicionei as fucnionalidades de armazenamento de utilizadoeres por input e por ficheiro .csv

--- P3G1_ProjetoFinal.cpp
+++ P3G1_ProjetoFinal.cpp
@@ -9,6 +9,10 @@ class User {
     string nome, email, telemovel;
     public:
     User(const string &nome, const string &email, const string &tele): nome(nome), email(email), telemovel(tele){}
+
+    string getNome() const { return nome; }
+    string getEmail() const { return email; }
+    string getTelemovel() const { return telemovel; }
 };
 
 class Msg {
@@ -44,9 +48,30 @@ class MobileMsg: public Msg{
 };
 int MobileMsg :: n = 1000;
 
-int main (){
+void InfoUser(vector<User> &utilizadores){
+    cout << "Introduza o nome do utilizador: ";
+    string nome;
+    cin >> nome;
 
-    ifstream ficheiro("utilizadores.csv");
+    cout << "Introduza o email do utilizador: ";
+    string email;
+    cin >> email;
+
+    cout << "Introduza o telemóvel do utilizador: ";
+    string telemovel;
+    cin >> telemovel;
+
+    User user(nome, email, telemovel);
+    utilizadores.push_back(user);
+    cout << "Nome: " << user.getNome() << ", Email: " << user.getEmail() << ", Telemóvel: " << user.getTelemovel() << endl;
+}
+
+void FileInput(vector<User> &utilizadores){
+
+    string file;
+    cin.clear();
+    cin >> file;
+    ifstream ficheiro(file);
     string linha;
     vector<User> listaUtilizadores;
 
@@ -63,9 +88,42 @@ int main (){
         }
         ficheiro.close();
     } else {
-        cerr << "Não foi possível abrir o ficheiro." << endl;
+        cout << "Não foi possível abrir o ficheiro." << endl;
     }
 
+    for (const auto &user : listaUtilizadores) {
+        cout << "Nome: " << user.getNome() << ", Email: " << user.getEmail() << ", Telemóvel: " << user.getTelemovel() << endl;
+    }
+
+}
+
+int main (){
+
+    int escolha;
+    vector<User> utilizadores;
+    do{
+    cout<< "Escolha como quer introduzir os dados: "<<endl;
+    cout<< "1 - A partir de um ficheiro CSV"<<endl;
+    cout<< "2 - A partir do input do utilizador"<<endl;
+    cin >> escolha;
+    switch (escolha)
+    {
+    case 1:
+    
+        FileInput(utilizadores);
+        
+        break;
+    
+    case 2:
+
+        InfoUser(utilizadores);
+
+        break;
+    }
+} while (escolha != 1 && escolha != 2);
+
+    
+    return 0;
     
 
 
