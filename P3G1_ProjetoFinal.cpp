f1f1cf16dc64892edf6ad32012eb6f810107af25
RodrigoOliveira <rodrigocastillo@ua.pt>
2026-05-08 19:02:39 +0100

    Adicionei implementacao de ficheiros no main

--- P3G1_ProjetoFinal.cpp
+++ P3G1_ProjetoFinal.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <vector>
 
 using namespace std;
@@ -25,14 +26,47 @@ class EmailMsg: public Msg{
     private:
     static int n;
     unsigned int ID;
+    string srcAddr, dstAddr;
 
     public:
-    EmailMsg(const string &info): Msg(info), ID(++n){}
+    EmailMsg(const string &info, const string &src, const string &dst): Msg(info), srcAddr(src), dstAddr(dst), ID(++n){}
 };
 int EmailMsg :: n = 1;
 
+class MobileMsg: public Msg{
+    private:
+    static int n;
+    unsigned int ID;
+    string srcPhoneNo, dstPhoneNo;
+
+    public:
+    MobileMsg(const string &info, const string &src, const string &dst): Msg(info), srcPhoneNo(src), dstPhoneNo(dst), ID(++n){}
+};
+int MobileMsg :: n = 1000;
 
 int main (){
+
+    ifstream ficheiro("utilizadores.csv");
+    string linha;
+    vector<User> listaUtilizadores;
+
+    if (ficheiro.is_open()) {
+        while (getline(ficheiro, linha)) {
+            size_t pos1 = linha.find(',');
+            size_t pos2 = linha.find(',', pos1 + 1);
+
+            string nome = linha.substr(0, pos1);
+            string email = linha.substr(pos1 + 1, pos2 - pos1 - 1);
+            string telemovel = linha.substr(pos2 + 1);
+
+            listaUtilizadores.emplace_back(nome, email, telemovel);
+        }
+        ficheiro.close();
+    } else {
+        cerr << "Não foi possível abrir o ficheiro." << endl;
+    }
+
+    
 
 
 
