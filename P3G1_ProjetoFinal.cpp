9a012afb9043820cc4cf48cb4d81c7eef650e438
Sara Batista <sarafvbatista@ua.pt>
2026-05-13 21:01:22 +0100

    Class User

--- P3G1_ProjetoFinal.cpp
+++ P3G1_ProjetoFinal.cpp
@@ -1,33 +1,38 @@
 #include <iostream>
-#include <vector>
+#include <string>
+#include "User.h"
 
 using namespace std;
 
-class User {
+/*class User {
     private:
     string nome, email, telemovel;
     public:
     User(const string &nome, const string &email, const string &tele): nome(nome), email(email), telemovel(tele){}
-};
+};*/
 
 class Msg {
     private:
-
     string info;
 
     public:
-
     Msg (const string &info): info(info){}
     virtual void getType()=0;
+    virtual ~Msg();
 };
 
-class EmailMsg: public Msg{
+class EmailMsg: virtual public Msg{
     private:
     static int n;
     unsigned int ID;
+    string srcAddr;
+    string dstAddr;
 
     public:
-    EmailMsg(const string &info): Msg(info), ID(++n){}
+    EmailMsg(const string &info, const string &s, const string &d): Msg(info), ID(++n), srcAddr(s), dstAddr(d){}
+    void getType() {
+        cout << "Mensagem enviada por email." << endl;
+    }
 };
 int EmailMsg :: n = 1;
 
