7938693801a8d7dcb8d77542839c6b22b739953a
Duarte Rodrigues <duartecrodrigues@ua.pt>
2026-05-14 14:39:06 +0100

    Começo do ponto 4

--- P3G1_ProjetoFinal.cpp
+++ P3G1_ProjetoFinal.cpp
@@ -57,6 +57,48 @@ void printInfo(vector <User> &utilizadores) {
         cout << "Nome: " << u.getNome() << ", Email: " << u.getEmail() << ", Telemóvel: " << u.getTelemovel() << endl;
     }
 }
+void InfoMsg(vector<Msg*> &mensagens) {
+    int tipo;
+    cout << "Tipo de mensagem (1-Email, 2-SMS): ";
+    cin >> tipo;
+    cin.ignore();
+    string info, src, dst;
+    cout << "Texto da mensagem: ";
+    getline(cin, info);
+    if (tipo == 1) {
+        cout << "Email de origem: "; getline(cin, src);
+        cout << "Email de destino: "; getline(cin, dst);
+        mensagens.push_back(new EmailMsg(info, src, dst));
+    } else if (tipo == 2) {
+        cout << "Telemovel de origem: "; getline(cin, src);
+        cout << "Telemovel de destino: "; getline(cin, dst);
+        mensagens.push_back(new MobileMsg(info, src, dst));
+    }
+}
+
+void FileMsgInput(vector<Msg*> &mensagens) {
+    string file;
+    cout << "Nome do ficheiro de mensagens CSV: ";
+    cin >> file;
+    ifstream ficheiro(file);
+    string linha;
+    if (ficheiro.is_open()) {
+        while (getline(ficheiro, linha)) {
+            size_t p1 = linha.find(',');
+            size_t p2 = linha.find(',', p1 + 1);
+            size_t p3 = linha.find(',', p2 + 1);
+            if (p1 != string::npos && p2 != string::npos && p3 != string::npos) {
+                string tipo = linha.substr(0, p1);
+                string info = linha.substr(p1 + 1, p2 - p1 - 1);
+                string src = linha.substr(p2 + 1, p3 - p2 - 1);
+                string dst = linha.substr(p3 + 1);
+                if (tipo == "email") mensagens.push_back(new EmailMsg(info, src, dst));
+                else if (tipo == "sms") mensagens.push_back(new MobileMsg(info, src, dst));
+            }
+        }
+        ficheiro.close();
+    }
+}
 
 
 
